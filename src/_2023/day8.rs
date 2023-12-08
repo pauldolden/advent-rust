1328f26b95c81dd7eac5c04ab512d52aa05027a0
Paul Dolden <paul.dolden@thefa.com>
2023-12-08 14:32:39 +0000

    day 8 part 2

---
+++ src/_2023/day8.rs
@@ -0,0 +1,66 @@
+use std::collections::HashMap;
+use std::fs;
+
+fn gcd(mut a: u64, mut b: u64) -> u64 {
+    while b != 0 {
+        let t = b;
+        b = a % b;
+        a = t;
+    }
+    a
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    a * b / gcd(a, b)
+}
+
+pub fn part_two() -> u64 {
+    let dir_map: HashMap<char, usize> = [('L', 0), ('R', 1)].into_iter().collect();
+    let mut map: HashMap<String, Vec<String>> = HashMap::new();
+    let input = fs::read_to_string("src/_2023/8.txt").unwrap();
+
+    let parts = input.split("\n\n").collect::<Vec<&str>>();
+    let instructions = parts[0]
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect::<Vec<char>>();
+
+    for line in parts[1].lines() {
+        let line_parts = line.split(" = ").collect::<Vec<&str>>();
+        let source = line_parts[0];
+        let dests = line_parts[1]
+            .split(", ")
+            .map(|d| d.trim_matches(|c: char| c == '(' || c == ')'))
+            .map(String::from)
+            .collect::<Vec<String>>();
+        map.insert(source.to_string(), dests);
+    }
+
+    let starting_nodes: Vec<String> = map
+        .keys()
+        .filter(|&key| key.ends_with('A'))
+        .cloned()
+        .collect();
+
+    let lengths = starting_nodes
+        .into_iter()
+        .map(|node| {
+            let mut steps = 0;
+            let mut current_node = node;
+            let mut i = 0;
+            while current_node.chars().last().unwrap() != 'Z' {
+                steps += 1;
+                let instruction = instructions[i];
+                let dir = dir_map[&instruction];
+                let curr_neighbors = &map[&current_node];
+                let next_node = &curr_neighbors[dir];
+
+                current_node = next_node.clone();
+                i = (i + 1) % instructions.len();
+            }
+            steps
+        })
+        .collect::<Vec<i32>>();
+
+    lengths.into_iter().fold(1, |n, x| lcm(x as u64, n))
+}
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,19 @@ pub mod _2023 {
     pub mod day1;
     pub mod day2;
     pub mod day3;
+    pub mod day8;
 }
 fn main() {
-    let d11 = crate::_2023::day1::part_one();
-    let d12 = crate::_2023::day1::part_two();
-    let d21 = crate::_2023::day2::part_one();
-    let d22 = crate::_2023::day2::part_two();
-    let d31 = crate::_2023::day3::part_one();
-    let d32 = crate::_2023::day3::part_two();
+    // let d11 = crate::_2023::day1::part_one();
+    // let d12 = crate::_2023::day1::part_two();
+    // let d21 = crate::_2023::day2::part_one();
+    // let d22 = crate::_2023::day2::part_two();
+    // let d31 = crate::_2023::day3::part_one();
+    let d81 = crate::_2023::day8::part_two();
 
-    println!("Day 1 Part 1: {}", d11);
-    println!("Day 1 Part 2: {}", d12);
-    println!("Day 2 Part 1: {}", d21);
-    println!("Day 2 Part 2: {}", d22);
-    println!("Day 3 Part 1: {}", d31);
-    println!("Day 3 Part 2: {}", d32);
+    // println!("Day 1 Part 1: {}", d11);
+    // println!("Day 1 Part 2: {}", d12);
+    // println!("Day 2 Part 1: {}", d21);
+    // println!("Day 2 Part 2: {}", d22);
+    println!("Day 8 Part 2: {}", d81);
 }
