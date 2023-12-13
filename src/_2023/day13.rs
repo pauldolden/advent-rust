1b58c655d5e6a4a1947a74086eec2022c2f8e497
Paul Dolden <paul.dolden@thefa.com>
2023-12-13 09:11:47 +0000

    solve part 1

--- src/_2023/day13.rs
+++ src/_2023/day13.rs
@@ -1,3 +1,4 @@
+use std::cmp::min;
 use std::fs;
 
 pub fn part_one() -> i64 {
@@ -14,42 +15,82 @@ pub fn part_one() -> i64 {
         })
         .collect::<Vec<Vec<Vec<char>>>>();
 
-    let values = find_symmetry(&grid);
+    let res = find_symmetry(&grid);
 
-    println!("{:?}", values);
-
-    0
+    res
 }
 
 fn find_symmetry(grid: &Vec<Vec<Vec<char>>>) -> i64 {
-    let mut horiz_values = Vec::new();
-    let mut vert_values = Vec::new();
-    for block in grid.iter() {
-        for (i, line) in block.iter().enumerate() {
-            if i < block.len() - 1 && line.clone() == block[i + 1] {
-                horiz_values.push((i) as i64);
-            }
-        }
-    }
+    let mut horizontal_symmetry = 0;
+    let mut vertical_symmetry = 0;
 
     for block in grid.iter() {
+        let mut found_symmetry = false;
+        for (i, line) in block.iter().enumerate() {
+            if i < block.len() - 1 && line.clone() == block[i + 1] {
+                let min_distance_to_edge = min(i, block.len() - 1 - i);
+                let left = i;
+                let right = i + 1;
+                let mut symmetrical = true;
+
+                for j in 0..=min_distance_to_edge {
+                    if left < j || right + j >= block.len() {
+                        break;
+                    }
+
+                    if block[left - j] != block[right + j] {
+                        symmetrical = false;
+                        break;
+                    }
+                }
+
+                if symmetrical {
+                    horizontal_symmetry += 100 * (i + 1);
+                    found_symmetry = true;
+                }
+            }
+        }
+
+        if found_symmetry {
+            continue;
+        }
+
+        let mut columns_as_rows = Vec::new();
+
         for col in 0..block[0].len() {
             let mut column_values = Vec::new();
             for row in 0..block.len() {
                 column_values.push(block[row][col]);
             }
-            for (i, line) in column_values.iter().enumerate() {
-                if i < column_values.len() - 1 && line.clone() == column_values[i + 1] {
-                    vert_values.push((i) as i64);
+            columns_as_rows.push(column_values);
+        }
+
+        for (i, line) in columns_as_rows.iter().enumerate() {
+            if i < columns_as_rows.len() - 1 && line.clone() == columns_as_rows[i + 1] {
+                let min_distance_to_edge = min(i, columns_as_rows.len() - 1 - i);
+                let left = i;
+                let right = i + 1;
+                let mut symmetrical = true;
+
+                for j in 0..=min_distance_to_edge {
+                    if left < j || right + j >= columns_as_rows.len() {
+                        break;
+                    }
+
+                    if columns_as_rows[left - j] != columns_as_rows[right + j] {
+                        symmetrical = false;
+                        break;
+                    }
+                }
+
+                if symmetrical {
+                    vertical_symmetry += i + 1;
                 }
             }
         }
     }
 
-    println!("{:?}", horiz_values);
-    println!("{:?}", vert_values);
-
-    0
+    (horizontal_symmetry + vertical_symmetry) as i64
 }
 
 pub fn part_two() -> i64 {
